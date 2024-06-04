e731c147ad809efeba0aefe536d751f75ccb5031
steph-sant <stephaniesantt07@gmail.com>
2024-06-04 12:03:11 -0300

    criação da classe conta terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+     Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+     //Exibir as mensagens para o nosso usuário
+     System.out.println("Por favor, digite o número da conta!");
+     int numero = scanner.nextInt();
+
+     System.out.println("Por favor, digite o numero da agência!");
+     String agencia = scanner.next();
+
+     System.out.println("Por favor, digite seu nome!");
+     String nomeCliente = scanner.next();
+
+     System.out.println("Por favor, digite o valor do saldo que deseja sacar!");
+     double saldo = scanner.nextDouble();
+
+     System.out.println("Olá," + nomeCliente + "" + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque!");
+    
     }
 }
